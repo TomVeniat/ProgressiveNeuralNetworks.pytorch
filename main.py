8a9b85429bd119bda68228a2856f7f5293bb1bc4
Tom Veniat <tom.veniat@lip6.fr>
2018-02-07 15:14:23 +0100

    Add Visdom integration

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import argparse
 
 import os
+
+import numpy as np
 import torch
 import torch.nn.functional as F
 
 import logging
+import visdom
 
 from torch.autograd import Variable
 from tqdm import tqdm
@@ -23,9 +26,11 @@ def get_args():
     parser = argparse.ArgumentParser(description='Progressive Neural Networks')
     parser.add_argument('-path', default='/local/veniat/data', type=str, help='path to the data')
     parser.add_argument('-cuda', default=-1, type=int, help='Cuda device to use (-1 for none)')
+    parser.add_argument('-visdom_url', default="http://37.187.126.101", type=str, help='Visdom server url')
+    parser.add_argument('-visdom_port', default=8100, type=int, help='Visdom server port')
 
-    parser.add_argument('--layers', metavar='L', type=int, default=4, help='Number of layers per task')
-    parser.add_argument('--sizes', dest='sizes', default=[784, 256, 256, 256, 10], nargs='+',
+    parser.add_argument('--layers', metavar='L', type=int, default=3, help='Number of layers per task')
+    parser.add_argument('--sizes', dest='sizes', default=[784, 1024, 512, 10], nargs='+',
                         action=LengthCheckAction)
 
     parser.add_argument('--n_tasks', dest='n_tasks', type=int, default=5)
@@ -41,6 +46,7 @@ def get_args():
 
 def main(args):
     os.environ['CUDA_VISIBLE_DEVICES'] = str(args['cuda'])
+    viz = visdom.Visdom(server=args['visdom_url'], port=args['visdom_port'], env='PNN tests')
 
     model = PNN(args['layers'])
 
@@ -65,6 +71,9 @@ def main(args):
 
         optimizer = torch.optim.RMSprop(model.parameters(task_id), lr=args['lr'],
                                         weight_decay=args['wd'], momentum=args['momentum'])
+
+        train_accs = []
+        train_losses = []
         for epoch in range(args['epochs']):
             total_samples = 0
             total_loss = 0
@@ -87,18 +96,28 @@ def main(args):
                 indiv_loss.backward()
                 optimizer.step()
 
+            train_accs.append(correct_samples / total_samples)
+            train_losses.append(total_loss / total_samples)
             logger.info(
-                '[T{}][{}/{}] Loss={}, Acc= {}'.format(task_id, epoch, args['epochs'], total_loss / total_samples,
-                                                       correct_samples / total_samples))
+                '[T{}][{}/{}] Loss={}, Acc= {}'.format(task_id, epoch, args['epochs'], train_losses[-1],
+                                                       train_accs[-1]))
+            viz.line(np.array(train_accs), X=np.arange(epoch+1), win='tacc{}'.format(task_id),
+                     opts={'title': 'Task {}: train accuracy'.format(task_id)})
+            viz.line(np.array(train_losses), X=np.arange(epoch+1), win='tloss{}'.format(task_id),
+                     opts={'title': 'Task {}: train loss'.format(task_id)})
 
         perfs = []
         logger.info('Evaluation after task {}:'.format(task_id))
-        for i in range(task_id+1):
+        for i in range(task_id + 1):
             _, val, test = tasks_data[i]
             val_perf = evaluate_model(model, x, y, val, task_id=i)
             test_perf = evaluate_model(model, x, y, test, task_id=i)
-            perfs.append((val_perf, test_perf))
+            perfs.append([val_perf, test_perf])
             logger.info('\tT n°{} - Val:{}%, test:{}%'.format(i, val_perf, test_perf))
+
+        viz.line(np.array(perfs), X=np.arange(task_id+1), win='all_task',
+                     opts={'title': 'Evaluation on all tasks', 'legend': ['Val', 'Test']})
+
 
 if __name__ == '__main__':
     main(vars(get_args()))
